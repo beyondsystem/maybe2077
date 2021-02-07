c1d91b0981ad34f08a8d7371326e2bc1bd2ce6fd
beyondsystem <contatobeyondsystem@gmail.com>
2021-02-07 11:29:50 -0300

    first_2

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,9 @@ function Resultado () {
     let peso = document.getElementById("peso").value;
     let altura = document.getElementById("altura").value;
     let imc = peso / ( altura * altura);
-    if (imc.entre(0, 18.5)) return document.getElementById("esse").value='Que tal fechar o lol ir comer?';
-    if (imc.entre(18.6, 24.9)) return document.getElementById("esse").value='Você aparenta estar com a saúde em dia';
-    if (imc.entre(25, 29.9)) return document.getElementById("esse").value='Massa magra ou gordura? você pode virar um bolinho';
-    if (imc.entre(30, 39.9)) return document.getElementById("esse").value='Aparentemente você é o Andre Marques antes da lipo';
-    if (imc.entre(40, 50)) return document.getElementById("esse").value="No way bro, you're dead";
+    if (imc.entre(0, 18.5)) document.getElementById("esse").value='Que tal fechar o lol ir comer?';
+    if (imc.entre(18.6, 24.9)) document.getElementById("esse").value='Você aparenta estar com a saúde em dia';
+    if (imc.entre(25, 29.9)) document.getElementById("esse").value='Massa magra ou gordura? você pode virar um bolinho';
+    if (imc.entre(30, 39.9)) document.getElementById("esse").value='Aparentemente você é o Andre Marques antes da lipo';
+    if (imc.entre(40, 50)) document.getElementById("esse").value="No way bro, you're dead";
 }
